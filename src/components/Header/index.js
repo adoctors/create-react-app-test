028c77503475827760cbdb7b9bccd306ff5e2e91
qinkeliang <qinkeliang@163.com>
2019-11-13 14:19:07 +0800

    init、 npm run eject、 add base.scss(webpack)

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.scss';
+
+
+const Header = () => {
+  return <div className="wrap">header-con</div>;
+}
+
+export default Header;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.scss';
+
+
+const Login = () => <div className="wrap">login-con</div>
+
+export default Login;
